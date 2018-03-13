45090e1b650b6130d8af74113d58d01b047586ef
Maik Luiz Paixão <paixomaik5@gmail.com>
2018-03-12 23:37:27 -0300

    Update Image_CLF.py

--- Image_CLF.py
+++ Image_CLF.py
@@ -4,8 +4,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 def build_arg_parser():
-    parser = argparse.ArgumentParser(description='Build a classifier using \
-            MNIST data')
+    parser = argparse.ArgumentParser(description='Build a classifier using MNIST data')
     parser.add_argument('--input-dir', dest='input_dir', type=str,
             default='./mnist_data', help='Directory for storing data')
     return parser
